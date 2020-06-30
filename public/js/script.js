9fa3a6dc63eaf95d20931dab8e0735c732b135b4
Bill (Paulo) Rocha <ahcordesign@gmail.com>
2020-06-30 13:40:28 -0300

    Add analytics

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,11 @@
-
-
 window.onload = () => {
-    console.log('Tuim.app is loaded!')
+	console.log('Tuim.app is loaded!')
 }
+
+window.dataLayer = window.dataLayer || []
+
+function gtag() {
+	dataLayer.push(arguments)
+}
+gtag('js', new Date())
+gtag('config', 'UA-171288641-1')
